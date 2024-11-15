d8c783a5ee8f5be6d4a605aec29fa468ea6cfada
san-shor <sanjidaewu89@gmail.com>
2024-11-15 14:40:41 +0600

    Get landing pad data

---
+++ src/lib/components/Table.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+  export let data = [];
+
+</script>
+
+<Table>
+  <TableHead>
+    <TableHeadCell>FULL NAME</TableHeadCell>
+    <TableHeadCell>LOCATION NAME</TableHeadCell>
+    <TableHeadCell>REGION</TableHeadCell>
+    <TableHeadCell>DETAILS</TableHeadCell>
+    <TableHeadCell>SUCCESS RATE</TableHeadCell>
+    <TableHeadCell>WIKIPEDIA LINK</TableHeadCell>
+    <TableHeadCell>STATUS</TableHeadCell>
+  </TableHead>
+  <TableBody tableBodyClass="divide-y">
+    <TableBodyRow>
+      {#each data as landingPads }
+      <TableBodyCell>{landingPads.full_name}</TableBodyCell>
+      <TableBodyCell>{landingPads.location}</TableBodyCell>
+      
+      {/each}
+      
+    </TableBodyRow>
+   
+ 
+  </TableBody>
+</Table>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,27 @@
 <script>
 import "../app.css"
 import Header from '$lib/components/Header.svelte';
+import Table from '$lib/components/Table.svelte';
+import { getLandingPadData } from './api/+server';
+import { onMount } from 'svelte';
+
+let landingPads = [];
+let error = null;
+
+  onMount(async () => {
+    try {
+      landingPads = await getLandingPadData();
+    } catch (err) {
+      error = err.message;
+      console.error('Error fetching landing pads:', error);
+    }
+  });
 </script>
 <Header/>
+<main class="p-4">
+ 
+ 
+    <Table data={landingPads} />
+
+</main>
 <slot></slot>
