a683f6aeee5b39aeea426ea029728bc61236ce75
san-shor <sanjidaewu89@gmail.com>
2024-11-16 10:37:31 +0600

    Integrate open layer map

---
+++ src/lib/components/MapView.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Card } from 'flowbite-svelte';
+  import Map from 'ol/Map.js';
+  import View from 'ol/View.js';
+  import TileLayer from 'ol/layer/Tile.js';
+  import OSM from 'ol/source/OSM.js';
+  import { onMount } from 'svelte';
+
+  let map;
+
+  onMount(() => {
+    map = new Map({
+      view: new View({
+        center: [0, 0],
+        zoom: 1,
+      }),
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: 'map',
+    });
+  });
+</script>
+
+<Card>
+  <div id="map" style="height: 400px; width: 100%;"></div>
+</Card>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script>
+  import MapView from '$lib/components/MapView.svelte';
   import Table from '$lib/components/Table.svelte';
+  import { Card } from 'flowbite-svelte';
+  import Map from 'ol/Map.js';
+  import View from 'ol/View.js';
+  import TileLayer from 'ol/layer/Tile.js';
+  import OSM from 'ol/source/OSM.js';
 
   let { data } = $props();
 </script>
@@ -8,5 +14,7 @@
   <div class="grid col-span-9">
     <Table data={data.landsPadData} />
   </div>
-  <div class="grid col-span-3">MapView</div>
+  <div class="grid col-span-3">
+    <MapView />
+  </div>
 </div>
