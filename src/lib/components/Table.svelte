116611339c7f43595a58c51e744ac68e2de0df4e
san-shor <sanjidaewu89@gmail.com>
2024-11-17 00:14:58 +0600

    Refactor table component

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -29,8 +29,8 @@
       <TableHeadCell>STATUS</TableHeadCell>
     </TableHead>
     <TableBody tableBodyClass="divide-y">
-      {#if filterValue === ''}
-        {#each data as landingPad}
+      {#each data as landingPad}
+        {#if filterValue === landingPad.status || filterValue === ''}
           <TableBodyRow class="text-sm font-semibold text-gray-900">
             <TableBodyCell>{landingPad.full_name}</TableBodyCell>
             <TableBodyCell>{landingPad.location.name}</TableBodyCell>
@@ -82,64 +82,8 @@
               ></TableBodyCell
             >
           </TableBodyRow>
-        {/each}
-      {:else}
-        {#each data as landingPad}
-          {#if filterValue === landingPad.status}
-            <TableBodyRow class="text-sm font-semibold text-gray-900">
-              <TableBodyCell>{landingPad.full_name}</TableBodyCell>
-              <TableBodyCell>{landingPad.location.name}</TableBodyCell>
-              <TableBodyCell>{landingPad.location.region}</TableBodyCell>
-              <TableBodyCell
-                ><Button
-                  color="light"
-                  size="sm"
-                  pill
-                  class="rounded-md bg-gray-100 text-xs font-medium py-[2px] px-[10px]"
-                  >View Details</Button
-                ></TableBodyCell
-              >
-              <TableBodyCell class="min-w-[150px]">
-                <Progressbar
-                  progress={landingPad.attempted_landings > 0
-                    ? (
-                        (landingPad.successful_landings /
-                          landingPad.attempted_landings) *
-                        100
-                      ).toFixed(0)
-                    : 0}
-                  size="h-1.5"
-                  progressClass="bg-green-400"
-                />
-                {landingPad.attempted_landings > 0
-                  ? (
-                      (landingPad.successful_landings /
-                        landingPad.attempted_landings) *
-                      100
-                    ).toFixed(0) + '%'
-                  : 'N/A'}</TableBodyCell
-              >
-              <TableBodyCell
-                ><a href={landingPad.wikipedia} target="_blank"
-                  ><LinkOutline color="#1C64F2" /></a
-                ></TableBodyCell
-              >
-              <TableBodyCell
-                ><Badge
-                  class="capitalize"
-                  color={`${
-                    landingPad.status === 'active'
-                      ? 'green'
-                      : landingPad.status === 'retired'
-                        ? 'red'
-                        : 'indigo'
-                  }`}>{landingPad.status}</Badge
-                ></TableBodyCell
-              >
-            </TableBodyRow>
-          {/if}
-        {/each}
-      {/if}
+        {/if}
+      {/each}
     </TableBody>
   </Table>
 </Card>
